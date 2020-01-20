022c979d718f10854c794f925abd3000f208391d
Shashwat Patel <35824714+shashwatpatel@users.noreply.github.com>
2020-01-20 14:03:38 -0800

    Add files via upload

---
+++ Pokemon/monster.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+#ifndef Monster_h
+#define Monster_h
+
+struct Monster
+{
+	string name;
+	int combatPower;
+};
+
+#endif
